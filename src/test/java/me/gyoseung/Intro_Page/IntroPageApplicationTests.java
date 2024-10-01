c8d92561faeaafccc106063c84227f19c9e37d62
Gyoseung Ku <ku@gyoseung.me>
2024-10-01 23:30:02 +0900

    File Upload

---
+++ src/test/java/me/gyoseung/Intro_Page/IntroPageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gyoseung.Intro_Page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
